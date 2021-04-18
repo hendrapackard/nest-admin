a6a24a7d008950fc93c729728999660aff63d510
hendra <hendra@ilcs.co.id>
2021-04-18 15:48:23 +0700

    * global prefix - prefix api - create controller & module auth nest g module auth nest g controller auth

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {UserModule} from './user/user.module';
 import {TypeOrmModule} from "@nestjs/typeorm";
+import {AuthModule} from './auth/auth.module';
 
 @Module({
     imports: [
@@ -16,7 +17,8 @@ import {TypeOrmModule} from "@nestjs/typeorm";
             database: 'nest_admin',
             autoLoadEntities: true,
             synchronize: true,
-        })],
+        }),
+        AuthModule],
     controllers: [AppController],
     providers: [AppService],
 })
