e7ebf35ee943b1fe7df693c1b637e7dbc67bf0d1
hendra <hendra@ilcs.co.id>
2021-04-18 15:19:36 +0700

    * modules & controller - create module user nest g module user - create controller user nest g controller user * migrations - install typeorm postgresql npm install --save @nestjs/typeorm typeorm pg - setting database - create entity user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {UserModule} from './user/user.module';
+import {TypeOrmModule} from "@nestjs/typeorm";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        UserModule,
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'postgres',
+            port: 5432,
+            username: 'nest_admin_user',
+            password: 'nest_admin_password',
+            database: 'nest_admin',
+            autoLoadEntities: true,
+            synchronize: true,
+        })],
+    controllers: [AppController],
+    providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
