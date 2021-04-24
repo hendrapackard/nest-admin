cd258d5357f7dc0890fab1f3ccf63be536ebbb3b
hendra <hendra@ilcs.co.id>
2021-04-24 19:48:22 +0700

    * Export CSV npm i json2csv @types/json2csv

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,6 +1,19 @@
-import {ClassSerializerInterceptor, Controller, Get, Query, UseGuards, UseInterceptors} from '@nestjs/common';
+import {
+    ClassSerializerInterceptor,
+    Controller,
+    Get,
+    Post,
+    Query,
+    Res,
+    UseGuards,
+    UseInterceptors
+} from '@nestjs/common';
 import {OrderService} from "./order.service";
 import {AuthGuard} from "../auth/auth.guard";
+import {Response} from "express";
+import {Parser} from "json2csv";
+import {Order} from "./models/order.entity";
+import {OrderItem} from "./models/order-item.entity";
 
 @UseInterceptors(ClassSerializerInterceptor)
 @UseGuards(AuthGuard)
@@ -12,5 +25,43 @@ export class OrderController {
     @Get('orders')
     async all(@Query('page') page = 1) {
         return this.orderService.paginate(page, ['order_items']);
+    }
+
+    @Post('export')
+    async export(@Res() res: Response) {
+        const parser = new Parser({
+            fields: ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']
+        });
+
+        const orders = await this.orderService.all(['order_items']);
+
+        const json = [];
+
+        orders.forEach((o: Order) => {
+            json.push({
+                ID: o.id,
+                Name: o.name,
+                Email: o.email,
+                'Product Title': '',
+                Price: '',
+                Quantity: ''
+            });
+
+            o.order_items.forEach((i: OrderItem) => {
+                json.push({
+                    ID: '',
+                    Name: '',
+                    Email: '',
+                    'Product Title': i.product_title,
+                    Price: i.price,
+                    Quantity: i.quantity
+                });
+            });
+        });
+
+        const csv = parser.parse(json);
+        res.header('Content-Type', 'text/csv');
+        res.attachment('orders.csv');
+        return res.send(csv);
     }
 }
